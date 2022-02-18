3f2cd9da74377e9e09ec199366be39f38acd90c5
Stoleoni <petar.stojanovic2@fer.hr>
2022-02-18 14:11:38 +0100

    Initial Commit

---
+++ PlayerAPI/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerAPI
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Club { get; set; } = string.Empty;
+        public string Nationality { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
